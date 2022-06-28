f90a4c4cf63c734dc1221bf6ccb318bff06b7d23
bj-jiwrajka <97835955+bj-jiwrajka@users.noreply.github.com>
2022-06-28 11:26:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    // console.log("Jai Shree Ganesh")
+    d = new Date();
+    htime= d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrot = 30*htime + mtime/2;
+    mrot = 6*mtime;
+    srot = 6*stime;
+    hour.style.transform = `rotate(${hrot}deg)`;
+    min.style.transform = `rotate(${mrot}deg)`;
+    sec.style.transform = `rotate(${srot}deg)`;
+}, 1000);
